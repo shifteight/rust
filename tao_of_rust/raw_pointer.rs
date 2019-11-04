3b64c81f4e9529a7328df9451de35334bdd1e6bb
Kevin Qian <scifi.org@gmail.com>
2019-11-04 17:08:02 +0800

    add tao_of_rust

---
+++ tao_of_rust/raw_pointer.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 10;
+    let ptr_x = &mut x as *mut i32;
+    let y = Box::new(20);
+    let ptr_y = &*y as *const i32;
+    println!("{:?} {:?}", ptr_x, ptr_y);
+    unsafe {
+        *ptr_x += *ptr_y;
+    }
+    assert_eq!(x, 30);
+}
