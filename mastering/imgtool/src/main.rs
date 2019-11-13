6eb2fbfe801ab5a08f23ef0fcec481fd4922e97a
Kevin Qian <scifi.org@gmail.com>
2019-11-13 15:43:58 +0800

    mastering rust, imgtool

--- mastering/imgtool/src/main.rs
+++ mastering/imgtool/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let image_path = env::args().skip(1).next().unwrap();
+    let path = Path::new(&image_path);
+
+    let parent = path.parent().unwrap();
+    let file_stem = path.file_stem().unwrap();
+    let extension = path.extension().unwrap();
+
+    let mut new_filename = file_stem.to_os_string().into_string().unwrap();
+    new_filename.push_str("_rotated.");
+    new_filename.push_str(extension.to_str().unwrap());
+    let out_path = parent.join(PathBuf::from(&new_filename));
+    
+    let img = image::open(path).unwrap();
+    let rotated = img.rotate90().rotate270();
+
+    rotated.save(out_path).unwrap();
 }
