227677ca914b1cd5fb5e91f7e99f3fbb86fc29c6
Kevin Qian <scifi.org@gmail.com>
2019-11-27 17:12:44 +0800

    error handling

--- rustbook/singles/common_play.rs
+++ rustbook/singles/common_play.rs
@@ -1,13 +1,1 @@
-use structopt::StructOpt;
 
-/// Search for a pattern in a file and display the lines containing it.
-#[derive(StructOpt)]
-struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
-
-fn main() {
-    let args = Cli::from_args();
-}
---
+++ rustbook/singles/error_kind.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
+}
+
+// 一个更老练的 Rustacean 可能会这么写:
+// fn main() {
+//     let f = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {:?}", error);
+//             })
+//         } else {
+//             panic!("Problem opening the file: {:?}", error);
+//         }
+//     });
+// }
